86f1327220831194432d55e06f1ebcfc265e47b7
Farrukh Normuradov <farrukh.normuradov@gmail.com>
2023-07-03 13:43:13 +0200

    feat: update fab and and back transition

--- app/src/main/java/com/normuradov/ajva/WordSearchActivity.kt
+++ app/src/main/java/com/normuradov/ajva/WordSearchActivity.kt
@@ -1,5 +1,6 @@
 package com.normuradov.ajva
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -10,13 +11,19 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.normuradov.ajva.screens.SearchScreen
 import com.normuradov.ajva.screens.WordDetailScreen
@@ -51,12 +58,17 @@ class WordSearchActivity : ComponentActivity() {
             viewModel.process(text)
 
             Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
+                modifier = Modifier
+                    .fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
             ) {
                 AjvaTheme {
                     DictionaryApp(
-                        viewModel = viewModel
+                        viewModel = viewModel,
+                        onCameraFABClick = {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
                     )
                 }
             }
@@ -70,34 +82,58 @@ class WordSearchActivity : ComponentActivity() {
 fun DictionaryApp(
     modifier: Modifier = Modifier,
     viewModel: SearchViewModel = viewModel(),
+    onCameraFABClick: () -> Unit = {},
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onCameraFABClick
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_photo_camera_100),
+                    contentDescription = "Camera"
+                )
+            }
+        }
+    ) { contentPadding ->
+        AnimatedVisibility(
+            visible = uiState.mode == SearchViewMode.Search,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            if (uiState.mode == SearchViewMode.Search) {
+                SearchScreen(modifier = Modifier.padding(contentPadding))
+            }
+        }
 
-    AnimatedVisibility(
-        visible = uiState.mode == SearchViewMode.Search,
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        if (uiState.mode == SearchViewMode.Search) {
-            SearchScreen()
+        AnimatedVisibility(
+            visible = uiState.mode == SearchViewMode.Detail,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            val selectedWord = uiState.selectedWord
+            if (uiState.mode == SearchViewMode.Detail && selectedWord != null) {
+                val coroutineScope = rememberCoroutineScope()
+
+                WordDetailScreen(
+                    modifier = Modifier.padding(contentPadding),
+                    word = selectedWord,
+                    onBackClick = {
+                        coroutineScope.launch {
+                            viewModel.navigateToSearchScreen()
+                        }
+                    })
+            }
         }
     }
+}
 
-
-    AnimatedVisibility(
-        visible = uiState.mode == SearchViewMode.Detail,
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        val selectedWord = uiState.selectedWord
-        if (uiState.mode == SearchViewMode.Detail && selectedWord != null) {
-            val coroutineScope = rememberCoroutineScope()
-            WordDetailScreen(word = selectedWord, onBackClick = {
-                coroutineScope.launch {
-                    viewModel.navigateToSearchScreen()
-                }
-            })
-        }
+@Preview
+@Composable
+fun DictionaryAppPreview() {
+    AjvaTheme {
+        DictionaryApp()
     }
 }
 
